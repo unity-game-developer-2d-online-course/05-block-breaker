3e384b9249a0bbe2e357156afbe9a90480adb78e
Mattias Strömberg <m.stromberg@outlook.com>
2020-11-04 17:02:19 +0100

    Move object with mouse

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float _screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * _screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
