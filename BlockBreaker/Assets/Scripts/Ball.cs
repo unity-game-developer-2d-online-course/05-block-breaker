8c332f214822b694bead2eee399d86a0c7c90e82
Mattias Strömberg <m.stromberg@outlook.com>
2020-11-05 22:38:56 +0100

    Distance as a Vector2

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Paddle _paddle;
+
+    private Vector2 _paddleToBallVector;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _paddleToBallVector = transform.position - _paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 paddlePos = new Vector2(_paddle.transform.position.x, _paddle.transform.position.y);
+        transform.position = paddlePos + _paddleToBallVector;
+    }
+}
