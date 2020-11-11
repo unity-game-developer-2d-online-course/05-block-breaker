d599d65c75b9f47e5dc9f82857c2d686dbee119e
Mattias Strömberg <m.stromberg@outlook.com>
2020-11-11 10:32:30 +0100

    Launch the ball

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -3,8 +3,11 @@
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle _paddle;
+    [SerializeField] private float _xPush = 2f;
+    [SerializeField] private float _yPush = 15f;
 
     private Vector2 _paddleToBallVector;
+    private bool _hasStarted = false;
 
     // Start is called before the first frame update
     private void Start()
@@ -15,7 +18,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (!_hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(_paddle.transform.position.x, _paddle.transform.position.y);
         transform.position = paddlePos + _paddleToBallVector;
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(_xPush, _yPush);
+            _hasStarted = true;
+        }
     }
 }
