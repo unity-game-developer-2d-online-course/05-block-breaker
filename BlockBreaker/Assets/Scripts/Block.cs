cdf1f36a98642e39cc1b16967da9e7570b8bbcd1
Mattias Strömberg <m.stromberg@outlook.com>
2021-05-10 16:16:36 +0200

    PlayClipAtPoint()

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -2,8 +2,11 @@
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip _breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(_breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
